f6f309149f763567e802cdb02e49ad95e46e2764
Stevö John <stevehuwjohn@gmail.com>
2019-10-04 14:10:13 +0100

    Adding tests

--- src/FightingFantasy.Engine.Tests/Core/DieTests.cs
+++ src/FightingFantasy.Engine.Tests/Core/DieTests.cs
@@ -1,10 +1,41 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FightingFantasy.Engine.Core;
+using NUnit.Framework;
 
 namespace FightingFantasy.Engine.Tests.Core
 {
     [TestFixture]
     public class DieTests
     {
-        
+        private IDie _die;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _die = new Die();
+        }
+
+        [Test]
+        public void Die_throws_between_one_and_six()
+        {
+            var rolls = new List<int>();
+
+            for (var i = 0; i < 100; i++)
+            {
+                var result = _die.Roll();
+
+                Assert.That(result, Is.InRange(1, 6));
+
+                rolls.Add(result);
+            }
+
+            Assert.True(rolls.Any(r => r == 1));
+            Assert.True(rolls.Any(r => r == 2));
+            Assert.True(rolls.Any(r => r == 3));
+            Assert.True(rolls.Any(r => r == 4));
+            Assert.True(rolls.Any(r => r == 5));
+            Assert.True(rolls.Any(r => r == 6));
+        }
     }
 }
---
+++ src/FightingFantasy.Engine.Tests/Models/ProtagonistAttributeTests.cs
@@ -0,0 +1,47 @@
+﻿using FightingFantasy.Engine.Models;
+using NUnit.Framework;
+
+namespace FightingFantasy.Engine.Tests.Models
+{
+    [TestFixture]
+    public class ProtagonistAttributeTests
+    {
+        private ProtagonistAttribute _protagonistAttribute;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _protagonistAttribute = new ProtagonistAttribute();
+        }
+
+        [Test]
+        public void HistoryLength_returns_correct_value()
+        {
+            _protagonistAttribute.Value = 1;
+            _protagonistAttribute.Value = 2;
+            _protagonistAttribute.Value = 3;
+
+            Assert.That(_protagonistAttribute.HistoryLength, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void InitialValue_returns_correct_value()
+        {
+            _protagonistAttribute.Value = 1;
+            _protagonistAttribute.Value = 2;
+            _protagonistAttribute.Value = 3;
+
+            Assert.That(_protagonistAttribute.InitialValue, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Value_returns_correct_value()
+        {
+            _protagonistAttribute.Value = 1;
+            _protagonistAttribute.Value = 2;
+            _protagonistAttribute.Value = 3;
+
+            Assert.That(_protagonistAttribute.Value, Is.EqualTo(3));
+        }
+    }
+}
