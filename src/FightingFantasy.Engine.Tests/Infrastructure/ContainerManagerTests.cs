534eb532248b26bf192e2c504d7424ae94abb37d
Stevö John <stevehuwjohn@gmail.com>
2019-10-04 14:46:31 +0100

    WIP

---
+++ src/FightingFantasy.Engine.Tests/Infrastructure/ContainerManagerTests.cs
@@ -0,0 +1,17 @@
+﻿using FightingFantasy.Engine.Infrastructure;
+using NUnit.Framework;
+
+namespace FightingFantasy.Engine.Tests.Infrastructure
+{
+    [TestFixture]
+    public class ContainerManagerTests
+    {
+        [Test]
+        public void Container_can_initialise_all_objects()
+        {
+            var container = ContainerManager.InitialiseContainer();
+
+            Assert.DoesNotThrow(() => container.Verify());
+        }
+    }
+}
