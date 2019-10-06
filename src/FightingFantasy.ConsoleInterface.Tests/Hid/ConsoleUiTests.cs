552b02eb9717ae7a35a98542d9b2e9fcd099070f
Stevö John <stevehuwjohn@gmail.com>
2019-10-06 17:09:07 +0100

    More WIP

--- src/FightingFantasy.ConsoleInterface.Tests/Hid/ConsoleUiTests.cs
+++ src/FightingFantasy.ConsoleInterface.Tests/Hid/ConsoleUiTests.cs
@@ -24,10 +24,49 @@ namespace FightingFantasy.ConsoleInterface.Tests.Hid
         [Test]
         public void Run_greets_the_user_appropriately()
         {
+            _input.Setup(i => i.ReadLine())
+                  .Returns("Exit");
+
             _consoleUi.Run();
 
             _output.Verify(o => o.Write("\nWelcome to Stevö John's <b>Fighting Fantasy</b> Game Engine!\n\n"));
-            _output.Verify(o => o.Write("Type <b>help</b> at any time for commands.\n\n"));
+            _output.Verify(o => o.Write("Type <b>help</b> at any time for a list of commands.\n\n"));
+        }
+
+        [Test]
+        public void Run_prompts_user_to_enter_an_instruction()
+        {
+            _input.SetupSequence(i => i.ReadLine())
+                  .Returns(" ")
+                  .Returns("Exit");
+
+            _consoleUi.Run();
+
+            _output.Verify(o => o.Write("\nPlease enter an instruction.\n\n"));
+        }
+
+        [Test]
+        public void Run_responds_appropriately_when_command_not_understood()
+        {
+            _input.SetupSequence(i => i.ReadLine())
+                  .Returns("Bugger off!")
+                  .Returns("Exit");
+
+            _consoleUi.Run();
+
+            _output.Verify(o => o.Write("\nI'm sorry, I don't know how to <b>bugger off!</b>.\n\n"));
+        }
+
+        [Test]
+        public void Run_shows_help_text()
+        {
+            _input.SetupSequence(i => i.ReadLine())
+                  .Returns("Help")
+                  .Returns("Exit");
+
+            _consoleUi.Run();
+
+            _output.Verify(o => o.Write(It.Is<string>(s => s.StartsWith("\nThe following commands"))));
         }
     }
 }
--- src/FightingFantasy.ConsoleInterface/Hid/ConsoleUi.cs
+++ src/FightingFantasy.ConsoleInterface/Hid/ConsoleUi.cs
@@ -16,7 +16,46 @@
             _output.Clear();
 
             _output.Write("\nWelcome to Stevö John's <b>Fighting Fantasy</b> Game Engine!\n\n");
-            _output.Write("Type <b>help</b> at any time for commands.\n\n");
+            _output.Write("Type <b>help</b> at any time for a list of commands.\n\n");
+
+            while (true)
+            {
+                var input = _input.ReadLine().Trim().ToLower();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    _output.Write("\nPlease enter an instruction.\n\n");
+                    continue;
+                }
+
+                switch (input)
+                {
+                    case "help":
+                        ShowHelpText();
+                        break;
+                    case "exit":
+                    case "quit":
+                    case "bye":
+                        _output.Write("\nThank you for playing, bye!\n\n");
+                        return;
+                    default:
+                        _output.Write($"\nI'm sorry, I don't know how to <b>{input}</b>.\n\n");
+                        break;
+                }
+            }
+        }
+
+        private void ShowHelpText()
+        {
+            _output.Write("\nThe following commands can be used at any time:\n\n");
+
+            _output.Write("  <b>Help</b> - Displays this list of commands.\n");
+            _output.Write("  <b>New</b>  - Start a new game.\n");
+            _output.Write("  <b>Load</b> - Load a saved game.\n");
+            _output.Write("  <b>Save</b> - Save the game so you can come back later.\n");
+            _output.Write("  <b>Exit</b> - Quit the game.\n");
+
+            _output.Write("\n");
         }
     }
 }
